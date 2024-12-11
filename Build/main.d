75851c7cc709d9766a04417e9fbe1571566a8331
M0iseevNikita <mns94.study@gmail.com>
2024-12-11 13:14:03 +0700

    update conf for nucleo_f767

---
+++ Build/main.d
@@ -0,0 +1,18 @@
+Build/main.o: Core/Src/main.c Core/Inc/init.h \
+ C:\Users\mns94\OneDrive\tpu\dmt\test\CMSIS\Devices\stm32f7xx.h \
+ C:\Users\mns94\OneDrive\tpu\dmt\test\CMSIS\Devices\stm32f767xx.h \
+ CMSIS/Include/core_cm7.h CMSIS/Include/cmsis_version.h \
+ CMSIS/Include/cmsis_compiler.h CMSIS/Include/cmsis_gcc.h \
+ CMSIS/Include/mpu_armv7.h \
+ C:\Users\mns94\OneDrive\tpu\dmt\test\CMSIS\Devices\system_stm32f7xx.h \
+ C:\Users\mns94\OneDrive\tpu\dmt\test\CMSIS\Devices\stm32f767xx.h
+Core/Inc/init.h:
+C:\Users\mns94\OneDrive\tpu\dmt\test\CMSIS\Devices\stm32f7xx.h:
+C:\Users\mns94\OneDrive\tpu\dmt\test\CMSIS\Devices\stm32f767xx.h:
+CMSIS/Include/core_cm7.h:
+CMSIS/Include/cmsis_version.h:
+CMSIS/Include/cmsis_compiler.h:
+CMSIS/Include/cmsis_gcc.h:
+CMSIS/Include/mpu_armv7.h:
+C:\Users\mns94\OneDrive\tpu\dmt\test\CMSIS\Devices\system_stm32f7xx.h:
+C:\Users\mns94\OneDrive\tpu\dmt\test\CMSIS\Devices\stm32f767xx.h:
---
+++ Core/Src/main.c
@@ -0,0 +1,7 @@
+#include <init.h>
+#include <stdint.h>
+float a = 12;
+int main(void)
+{
+        a = 0.11+22.1;
+}
